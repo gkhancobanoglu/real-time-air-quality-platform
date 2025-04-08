00cbc86c1fc45d475a2141ff589dcf06866464c2
Gökhan Çobanoğlu <gokhan@larsca.com>
2025-04-08 22:29:40 +0300

    feat(air-data-service) : exception

---
+++ air-data-service/src/main/java/com/cobanoglu/airdataservice/exception/AirQualityFetchException.java
@@ -0,0 +1,7 @@
+package com.cobanoglu.airdataservice.exception;
+
+public class AirQualityFetchException extends RuntimeException {
+    public AirQualityFetchException(String message) {
+        super(message);
+    }
+}
