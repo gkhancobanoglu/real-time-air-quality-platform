b67101f7f7add91a0b5eba836168ee28ddceab9e
Gökhan Çobanoğlu <gokhan@larsca.com>
2025-04-28 22:42:06 +0300

    feat: add findByTimestampBetween query method in AnomalyRepository

--- anomaly-detection-service/src/main/java/com/cobanoglu/anomalydetectionservice/repository/AnomalyRepository.java
+++ anomaly-detection-service/src/main/java/com/cobanoglu/anomalydetectionservice/repository/AnomalyRepository.java
@@ -4,6 +4,10 @@ import com.cobanoglu.anomalydetectionservice.model.Anomaly;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AnomalyRepository extends JpaRepository<Anomaly, Long> {
+
+    List<Anomaly> findByTimestampBetween(Long startTimestamp, Long endTimestamp);
 }
