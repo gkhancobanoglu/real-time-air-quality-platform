13967b82205ef1b292d6465de8b2e462787f3ea3
Gökhan Çobanoğlu <gokhan@larsca.com>
2025-04-28 17:36:51 +0300

    Add AnomalyController for anomaly data

---
+++ anomaly-detection-service/src/main/java/com/cobanoglu/anomalydetectionservice/controller/AnomalyController.java
@@ -0,0 +1,21 @@
+package com.cobanoglu.anomalydetectionservice.controller;
+
+import com.cobanoglu.anomalydetectionservice.model.Anomaly;
+import com.cobanoglu.anomalydetectionservice.service.AnomalyDetectionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/anomalies")
+@RequiredArgsConstructor
+public class AnomalyController {
+
+    private final AnomalyDetectionService anomalyDetectionService;
+
+    @GetMapping
+    public List<Anomaly> getAllAnomalies() {
+        return anomalyDetectionService.getAllAnomalies();
+    }
+}
