e0d771743231ee07f7799a1b8642f04814aa10f0
Gökhan Çobanoğlu <gokhan@larsca.com>
2025-05-04 23:12:38 +0300

    Implement AirQualityController to handle air quality data requests

--- air-data-service/src/main/java/com/cobanoglu/airdataservice/controller/AirQualityController.java
+++ air-data-service/src/main/java/com/cobanoglu/airdataservice/controller/AirQualityController.java
@@ -1,6 +1,8 @@
 package com.cobanoglu.airdataservice.controller;
 
+import com.cobanoglu.airdataservice.dto.AirDataRequest;
 import com.cobanoglu.airdataservice.model.AirQualityResponse;
+import com.cobanoglu.airdataservice.model.PollutantData;
 import com.cobanoglu.airdataservice.service.AirQualityService;
 import com.cobanoglu.airdataservice.service.KafkaProducerService;
 import jakarta.validation.constraints.NotNull;
@@ -8,6 +10,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/air")
@@ -28,4 +34,48 @@ public class AirQualityController {
 
         return ResponseEntity.ok(response);
     }
+
+    @PostMapping("/data")
+    public ResponseEntity<Void> submitAirData(@RequestBody AirDataRequest request) {
+        AirQualityResponse.AirData data = new AirQualityResponse.AirData();
+        data.setLat(request.getLatitude());
+        data.setLon(request.getLongitude());
+        data.setDt(System.currentTimeMillis());
+
+        PollutantData components = new PollutantData();
+        switch (request.getParameter()) {
+            case "PM2.5":
+                components.setPm2_5(request.getValue());
+                break;
+            case "PM10":
+                components.setPm10(request.getValue());
+                break;
+            case "NO2":
+                components.setNo2(request.getValue());
+                break;
+            case "SO2":
+                components.setSo2(request.getValue());
+                break;
+            case "O3":
+                components.setO3(request.getValue());
+                break;
+        }
+
+        data.setComponents(components);
+
+        AirQualityResponse.MainData main = new AirQualityResponse.MainData();
+        main.setAqi(1);
+        data.setMain(main);
+
+        AirQualityResponse response = new AirQualityResponse();
+        List<AirQualityResponse.AirData> list = new ArrayList<>();
+        list.add(data);
+        response.setList(list);
+
+        kafkaProducerService.sendAirQualityData(response);
+        return ResponseEntity.ok().build();
+    }
+
+
+
 }
