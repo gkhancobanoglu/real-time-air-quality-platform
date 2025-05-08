f9b09be1317f577a893c6c78a929f1a827f3bafc
Gökhan Çobanoğlu <gokhan@larsca.com>
2025-05-08 18:30:36 +0300

    Added parameters

--- backend/air-data-service/src/main/java/com/cobanoglu/airdataservice/dto/AirDataRequest.java
+++ backend/air-data-service/src/main/java/com/cobanoglu/airdataservice/dto/AirDataRequest.java
@@ -4,8 +4,14 @@ import lombok.Data;
 
 @Data
 public class AirDataRequest {
-    private double latitude;
-    private double longitude;
-    private String parameter;
-    private double value;
+    private Double latitude;
+    private Double longitude;
+    private Double co;
+    private Double no;
+    private Double no2;
+    private Double o3;
+    private Double so2;
+    private Double pm2_5;
+    private Double pm10;
+    private Double nh3;
 }
